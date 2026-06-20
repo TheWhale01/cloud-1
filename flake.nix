1a92302e7c3d446fbaa777689070223a5a4c7503
TheWhale01 <ard.rasp01@gmail.com>
2026-06-20 21:07:21 +0200

    first commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Cloud-1";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              ansible
+            ];
+          };
+        }
+      );
+    };
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Could-1
+
+This project is about auto deployment and configuration of environments. It uses [Ansible](https://github.com/ansible/ansible) for this job.
+
+## How to run ?
+
+Using Nix:
+
+```bash
+nix develop
+```
+
+You can run this on other distributions. Please refer to the Ansible [documentation](https://docs.ansible.com/) for that
